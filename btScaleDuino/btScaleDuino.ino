e3d440246ad197d8122871c6103251ba5530449a
Lars Rademacher <rademacher.lars@gmx.de>
2017-03-31 16:27:11 +0200

    Added SSD1331 display and fundamental displaying of measured weight

--- btScaleDuino/btScaleDuino.ino
+++ btScaleDuino/btScaleDuino.ino
@@ -3,21 +3,46 @@
 #include <Filters.h>
 #include <SoftwareSerial.h>
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1331.h>
+#include <SPI.h>
+
 SoftwareSerial BTserial(2, 3);
 
 #define DEBUG 1
 #define VERSION_STRING "0.01"
+#define FLIP_DISPLAY 0
+// Color definitions
+#define  BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
+// You can use any (4 or) 5 pins 
+#define sclk 13
+#define mosi 11
+#define cs   10
+#define rst  9
+#define dc   8
+
+Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);
 
 // Scale Settings
 const int SCALE_DOUT_PIN = A1;
 const int SCALE_SCK_PIN = A0;
 
 const unsigned long updateRateMs = 200;
+const unsigned long updateRateMsDisp = 500;
 const unsigned int maxNumSamples = 50;
 
 float meanSum;
 unsigned int meanNumSamples;
 unsigned long lastTimeUpdate;
+unsigned long lastTimeUpdateDisp;
 
 // Low pass filter
 // filters out changes faster that 5 Hz.
@@ -61,11 +86,20 @@ void addMeanSample(float val);
 void receiveCommands(void);
 
 void setupBluetoothModule(void);
+void setupDisplay(void);
+
+void printBluetoothLogo(bool connected);
+void printStatus(float weight, bool connected);
 
 // Functions
 void setup() {
   Serial.begin(9600);
   Serial.println("btScaleDuino v" VERSION_STRING);
+
+  setupDisplay();
+
+  display.fillScreen(GREEN);
+  display.print("Initializing...");
   
   setupBluetoothModule();
 
@@ -78,7 +112,11 @@ void setup() {
   lastTimeUpdate = millis();
 
   printInfo();
+
+  display.fillScreen(BLACK);
 }
+
+float lastValidMeasurement = 0.0f;
 
 void loop() {
   unsigned long currentMs = millis();
@@ -89,7 +127,15 @@ void loop() {
     BTserial.print(CMD_VALUE_CHAR);
     BTserial.print(String(meanVal, 2));
     BTserial.print(EOS_CHAR);
+
+    lastValidMeasurement = meanVal;
     lastTimeUpdate = currentMs;
+  }
+
+  if ( ( currentMs - lastTimeUpdateDisp ) > updateRateMsDisp )
+  {
+    printStatus(lastValidMeasurement, true);
+    lastTimeUpdateDisp = currentMs;
   }
 
   //addMeanSample(scale.get_units(1));
@@ -98,6 +144,49 @@ void loop() {
   receiveCommands();
 }
 
+void setupDisplay(void) {
+  display.begin();
+#if FLIP_DISPLAY
+  display.setRotation(2);
+#endif
+  display.fillScreen(BLACK);
+}
+
+void printBluetoothLogo(bool connected)
+{
+  uint16_t col;
+  if(connected)
+  {
+    col = GREEN;
+  }
+  else
+  {
+    col = RED;
+  }
+  display.drawLine(9, 1, 9, 18, col);
+  display.drawLine(5, 5, 14, 13, col);
+  display.drawLine(5, 14, 14, 5, col);
+  display.drawLine(9, 1, 14, 5, col);
+  display.drawLine(9, 18, 14, 13, col);
+}
+
+void printStatus(float weight, bool connected)
+{
+  uint16_t preComma, postComma;
+  preComma = (uint16_t) weight;
+  postComma = (uint16_t)(((uint32_t) (weight*100.0f))%100);
+  
+  //display.fillScreen(BLACK);
+  display.setTextColor(WHITE, BLACK);
+  char weightStr[10];
+  display.setCursor(0, 30);
+  display.setTextSize(2);
+  /* TODO: Print leading space in case of positive number. */
+  sprintf(weightStr, "%4d.%02dg", preComma, postComma);
+  display.print(weightStr);
+
+  printBluetoothLogo(connected);
+}
 
 void setupBluetoothModule(void) {
   BTserial.begin(9600);
